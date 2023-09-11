68ae8e4febf110c540a2bf0702579d319727d5b1
Qerced <114247529+Qerced@users.noreply.github.com>
2023-09-11 20:11:36 +0300

    Update db.py

--- app/core/db.py
+++ app/core/db.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, MetaData
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, declared_attr, sessionmaker
-from sqlalchemy import MetaData
 
 from app.core.config import settings
 
