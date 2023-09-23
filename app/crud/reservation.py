34a654044e157a5a771e8d020771ec969070af4d
Вакаускас Витас <ivitasvakayskas@gmail.com>
2023-09-23 21:09:01 +0300

    add google api

--- app/crud/reservation.py
+++ app/crud/reservation.py
@@ -1,6 +1,7 @@
 import datetime
 from typing import Optional
-from sqlalchemy import select, and_
+
+from sqlalchemy import and_, func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
@@ -53,6 +54,21 @@ class CRUDReservation(CRUDBase):
             )
         )
         return reservations.scalars().all()
+
+    async def get_count_res_at_the_same_time(
+            self,
+            from_reserve: datetime,
+            to_reserve: datetime,
+            session: AsyncSession
+    ) -> list[dict[str, int]]:
+        reservations = await session.execute(
+            select([Reservation.meetingroom_id,
+                    func.count(Reservation.meetingroom_id)]).where(
+                Reservation.from_reserve >= from_reserve,
+                Reservation.to_reserve <= to_reserve
+            ).group_by(Reservation.meetingroom_id)
+        )
+        return reservations.all()
 
 
 reservation_crud = CRUDReservation(Reservation)
